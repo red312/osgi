c2472683d59e2fd5df54a48771a1fe2a27208f5d
red312 <orman.213@yandex.ru>
2022-03-03 19:26:01 +0300

    some fixes

--- secondTask/helloClient/src/main/java/hello/client/Client.java
+++ secondTask/helloClient/src/main/java/hello/client/Client.java
@@ -3,10 +3,14 @@ package hello.client;
 import hello.service.Greetings;
 import org.osgi.framework.*;
 public class Client implements BundleActivator {
+
+    @Override
     public void start(BundleContext ctx) {
         ServiceReference<?> ref =
                 ctx.getServiceReference(Greetings.class.getName());
         ((Greetings) ctx.getService(ref)).sayHello();
     }
+
+    @Override
     public void stop(BundleContext ctx) {}
 }
--- secondTask/helloService/src/main/java/hello/service/impl/Activator.java
+++ secondTask/helloService/src/main/java/hello/service/impl/Activator.java
@@ -4,9 +4,13 @@ import hello.service.Greetings;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 public class Activator implements BundleActivator{
+
+    @Override
     public void start(BundleContext ctx) {
         ctx.registerService(Greetings.class.getName(),
                 new GreetingsImpl(), null);
     }
+
+    @Override
     public void stop(BundleContext ctx) {}
 }
