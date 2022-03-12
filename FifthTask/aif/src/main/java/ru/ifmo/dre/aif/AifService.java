b4a965adcc8bba3ed30f891a3530009ba48339ea
red312 <orman.213@yandex.ru>
2022-03-12 17:26:21 +0300

    add AIF

---
+++ FifthTask/aif/src/main/java/ru/ifmo/dre/aif/AifService.java
@@ -0,0 +1,37 @@
+package ru.ifmo.dre.aif;
+
+import com.rometools.rome.feed.synd.*;
+import com.rometools.rome.io.*;
+import ru.ifmo.dre.service.AbstractNews;
+import ru.ifmo.dre.service.NewsService;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+public class AifService extends AbstractNews implements NewsService  {
+
+    public String getNewsName(){
+        return "AIF";
+    }
+
+    public List<String> getAllWords() {
+        List<String> res = new ArrayList<>();
+        try {
+            URL url = new URL("https://www.aif.ru/rss/news.php");
+            SyndFeedInput input = new SyndFeedInput();
+            SyndFeed feed = input.build(new XmlReader(url));
+            List<SyndEntry> entries = feed.getEntries();
+            for (SyndEntry entry : entries) {
+                res.addAll(Stream.of(entry.getTitle().split("[^A-Za-zА-Яа-я]+"))
+                        .map(String::toLowerCase).toList());
+            }
+        } catch (IOException | FeedException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+}
